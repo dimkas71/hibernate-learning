912e26a3f1651cca148cd0f6b04d77a103b5f480
dimkas71g@gmail.com <dimkas71g@gmail.com>
2019-05-10 11:27:14 +0300

    Add demo using session for quering requests to the db

--- src/main/java/ua/selftaught/HibernateSessionApp.java
+++ src/main/java/ua/selftaught/HibernateSessionApp.java
@@ -5,15 +5,26 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ua.selftaught.entity.Employee;
+import ua.selftaught.entity.HartTrophyWinners;
+import ua.selftaught.entity.User;
 
 public class HibernateSessionApp {
-
+	
+	private static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("ua.selftaught.hibernate.jpa");
+	
 	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 	
@@ -24,7 +35,28 @@ public class HibernateSessionApp {
 		
 		List<Employee> e = mapper.readValue(userDir.resolve("employees.json").toFile(),tr);
 		
-		System.out.println(e);
+		
+		SessionFactory sf = EMF.unwrap(SessionFactory.class);
+		
+		
+		Session session = sf.openSession();
+		
+		Query<User> query = session.createQuery("Select u from User u");
+		
+		List<User> users = query.getResultList();
+		
+		Query<HartTrophyWinners> q = session.createQuery("Select htw from HartTrophyWinners htw");
+
+		List<HartTrophyWinners> htws = q.getResultList();
+		
+		System.out.println(users);
+		
+		System.out.println(htws);
+		
+		
+		session.close();
+		
+		sf.close();
 		
 		
 	}
