e46edc30e4366cb5636aef36489b34628133ff92
dimkas71g@gmail.com <dimkas71g@gmail.com>
2019-05-17 18:23:18 +0300

    Add password converter

--- src/main/java/ua/selftaught/HibernateSessionApp.java
+++ src/main/java/ua/selftaught/HibernateSessionApp.java
@@ -1,8 +1,11 @@
 package ua.selftaught;
 
-import java.io.IOException;
+import java.security.Key;
+import java.util.Base64;
 import java.util.List;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Convert;
 import javax.persistence.Converter;
@@ -17,17 +20,8 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToOne;
 import javax.persistence.Persistence;
-import javax.persistence.Tuple;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaDelete;
-import javax.persistence.criteria.CriteriaUpdate;
-import javax.persistence.criteria.Root;
 
-import org.hibernate.cfg.AttributeConversionInfo;
 import org.jboss.logging.Logger;
-
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -40,50 +34,22 @@ public class HibernateSessionApp {
 	
 	private static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("ua.selftaught.hibernate.jpa");
 	
-	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+	public static void main(String[] args) {
 		
 		
 		final EntityManager em = EMF.createEntityManager();
 		
 		
-		List<Object[]> users = em.createNativeQuery("select u.name, u.password from user1 u where u.id > ?")
-			.setParameter(1, 3)	
-			.getResultList();
-		
-		for (Object[] o : users) {
-			log.infov("{0}", new Pair<Object, Object>(o[0], o[1]));
-		}
-
-		
-		List<Tuple> results = em.createNativeQuery("select u.name as name, u.password as password from User1 u", Tuple.class)
-			.getResultList();
-
-		for (Tuple t : results) {
-			log.infov("Name {0}, password {1}", t.get("name"), t.get("password"));
-		}
-		
 		em.getTransaction().begin();
+		User u = em.find(User.class, 5);
 		
-		CriteriaBuilder cb = em.getCriteriaBuilder();
+		u.setPassword("secret3");
 		
-		CriteriaUpdate<User> update = cb
-			.createCriteriaUpdate(User.class);
-			
-		Root<User> root = update.from(User.class);
-		
-		update.set("password", "prettl");
-		update.where(cb.equal(root.get("id"), 1));
-		
-		em.createQuery(update).executeUpdate();
-		
-		
-		CriteriaDelete<User> delete = cb.createCriteriaDelete(User.class);
-		Root<User> rootDelete = delete.from(User.class);
-		
-		delete.where(cb.equal(rootDelete.get("id"), 2));
-		em.createQuery(delete).executeUpdate();
+		em.persist(u);
 		
 		em.getTransaction().commit();
+		
+		
 		
 		em.close();
 		
@@ -160,17 +126,34 @@ class User {
 }
 
 
-@Converter(autoApply = true)
+@Converter(autoApply = false)
 class PasswordConverter implements AttributeConverter<String, String> {
 
+	private static final String Algorithm = "AES/ECB/PKCS5Padding";
+	private static final byte[] KEY = "MySuperSecretKey".getBytes();
+	
 	@Override
 	public String convertToDatabaseColumn(String attribute) {
-		return "abc";
+		Key key = new SecretKeySpec(KEY, "AES");
+		try {
+			Cipher cipher = Cipher.getInstance(Algorithm);
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			return Base64.getEncoder().encodeToString(cipher.doFinal(attribute.getBytes()));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public String convertToEntityAttribute(String dbData) {
-		return "ddd";
+		Key key = new SecretKeySpec(KEY, "AES");
+		try {
+			Cipher cipher = Cipher.getInstance(Algorithm);
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			return new String(Base64.getDecoder().decode(dbData.getBytes()));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 }
