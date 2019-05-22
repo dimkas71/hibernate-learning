fcd52bd877656286d35f92bde9a284271cc23294
dimkas71g@gmail.com <dimkas71g@gmail.com>
2019-05-22 21:55:32 +0300

    Add new persistence unit, configuring EntityManagerFactory with props.

--- src/main/java/ua/selftaught/HibernateApp.java
+++ src/main/java/ua/selftaught/HibernateApp.java
@@ -2,63 +2,50 @@ package ua.selftaught;
 
 import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.jboss.logging.Logger;
 
-import ua.selftaught.entity.compositeid.Book;
-import ua.selftaught.entity.compositeid.Library;
+import ua.selftaught.entity.northwind.Customer;
 
 public class HibernateApp {
 private static final Logger log =  Logger.getLogger(HibernateSessionApp.class);
 	
-	private static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("ua.selftaught.hibernate.jpa.h2");
+	private static final EntityManagerFactory EMF;
+	static {
+		
+		Map<String, String> props = new HashMap<>();
+		
+		props.put("javax.persistence.jdbc.url", "jdbc:h2:tcp://localhost/~/test");
+		props.put("javax.persistence.jdbc.driver","org.h2.Driver");
+		props.put("javax.persistence.jdbc.user", "sa");
+		props.put("javax.persistence.jdbc.password", "");
+		props.put("hibernate.show_sql", "true");
+		props.put("hibernate.hbm2ddl.auto", "update");
+		props.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		
+		EMF = Persistence.createEntityManagerFactory("ua.selftaught.hibernate.jpa.h3", props);
+	}
 	
 	public static void main(String[] args) {
 			
 		doInJPA(() -> EMF, em -> {
 			
-			Library lib = em.find(Library.class, 1);
-			
-			log.infov("{0}", lib);
-			
-			Book b1 = em.find(Book.class, 2L);
-			
-			lib.getBooks().add(b1);
-			
-			em.persist(lib);
-			
-			//lib.getBooks().add(b1);
-			
-			//em.persist(lib);
-			
-			//Library lib =em.find(Library.class, 1);
+			List<Customer> customers = em.createQuery("select c from Customer c", Customer.class)
+				.getResultList();
 			
 			
-			//em.persist(lib);
+			log.infov("{0}", "Hello JPA");
 			
-			//List<Book> books =  em.createQuery("select b from Book b", Book.class)
-			//		.getResultList();
-		
-			
-			//Library lib = em.createQuery("select l from Library l where l.id = :id", Library.class)
-			//	.setParameter("id", 1)
-			//	.getSingleResult();
-			
-			
-			
-			//log.infov("Books : {0}", books);
-			
-			//log.infov("Library : {0}", lib);
-			
-			//em.remove(lib);
+			log.infov("Customers : {0}", customers);
 			
 		});
 		
-		doInJPA(() -> EMF, em -> {
-			
-		});
 	
 	}
 
