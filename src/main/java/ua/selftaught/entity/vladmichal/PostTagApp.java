dff5df9ba02f8625a0e2d181c5cf94fc4fe73030
dimkas71g@gmail.com <dimkas71g@gmail.com>
2019-05-23 23:03:53 +0300

    Remove misc tag from the first post

--- src/main/java/ua/selftaught/entity/vladmichal/PostTagApp.java
+++ src/main/java/ua/selftaught/entity/vladmichal/PostTagApp.java
@@ -2,8 +2,6 @@ package ua.selftaught.entity.vladmichal;
 
 import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
 
-import java.util.List;
-
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -17,67 +15,24 @@ public class PostTagApp {
 	private static final Logger log = Logger.getLogger(PostTagApp.class);
 	
 	public static void main(String[] args) {
-		/*
-		Tag misc = new Tag("Misc");
-		Tag jdbc = new Tag("JDBC");
-		Tag jooq = new Tag("JOOQ");
-		Tag hibernate = new Tag("Hibernate");
-		*/
 		
 		doInJPA(()-> EMF, em -> {
-			/*
-			em.persist(misc);
-			em.persist(jdbc);
-			em.persist(jooq);
-			em.persist(hibernate);
-			*/
-			
-			List<Tag> tags = em.createQuery("Select t from Tag t", Tag.class)
-				.getResultList();
-			
-			log.infov("Tags : {0}", tags);
 			
 			
 			Session session = em.unwrap(Session.class);
 			
 			Tag misc = session.bySimpleNaturalId(Tag.class)
-				.load("Misc");
+					.load("Misc");
 			
-			Tag hibernate = session.bySimpleNaturalId(Tag.class)
-				.load("Hibernate");
+			log.infov("{0}", misc.getName());
 			
-			Tag jooq = session.bySimpleNaturalId(Tag.class)
-					.load("JOOQ");
+			Post p = em.createQuery("select p from Post p where p.id = :id", Post.class)
+				.setParameter("id", 13)
+				.getSingleResult();
 			
-			Tag jdbc = session.bySimpleNaturalId(Tag.class)
-					.load("JDBC");
-					
+			log.infov("{0}", p.getTitle());
 			
-			log.infov("{0}", jdbc);
-			
-			
-			Post hpjp1 = new Post("High-Performance Java Persistence 1st edition");
-			
-			hpjp1.setId(1);
-			
-			hpjp1.addTag(misc);
-			hpjp1.addTag(jdbc);
-			hpjp1.addTag(jooq);
-			hpjp1.addTag(hibernate);
-			
-			session.save(hpjp1);
-			
-			Post hpjp2 = new Post(
-				    "High-Performance Java Persistence 2nd edition"
-				);
-				hpjp2.setId(2);
-				 
-				hpjp2.addTag(jdbc);
-				hpjp2.addTag(hibernate);
-				hpjp2.addTag(jooq);
-				 
-				session.save(hpjp2);
-			
+			p.removeTag(misc);
 			
 		});
 		
